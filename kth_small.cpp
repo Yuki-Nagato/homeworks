83cf63e3e22fce870d118b64dce79abf4a8dbcad
長門有希 <Yuki-Nagato@users.noreply.github.com>
2018-06-30 21:49:27 +0800

    Update kth_small.cpp

--- kth_small.cpp
+++ kth_small.cpp
@@ -30,12 +30,15 @@ int kth_small(int num[], int len, int k) {
 
 int main() {
     using namespace std;
-    while(cin >> n) {
+    while(cout << "数据个数" << endl && cin >> n) {
+        cout << "数据" << endl;
         for(int i=0; i<n; i++) {
             cin >> a[i];
         }
-        cout << "Max: " << kth_small(a, n, n) << endl;
-        cout << "Middle: " << double(kth_small(a, n, (n+1)/2)+kth_small(a,n,(n+2)/2))/2 << endl;
+        cout << "输入k" << endl;
+        int k;
+        cin >> k;
+        cout << "第k小的数是" << kth_small(a, n, k) << endl;
     }
     return 0;
 }
